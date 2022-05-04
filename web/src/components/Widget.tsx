0e7f84f803147decd15b0fd7a3896b5dc4005f10
Jonathan Araujo <jonathan.x939@gmail.com>
2022-05-04 01:04:42 -0300

    Segundo dia finalizado

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,6 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react';
+import { WidgetForm } from './WidgetForm';
 // import { useState } from 'react';
 
 // Estado - React observa as alterações das variaveis
@@ -13,8 +14,10 @@ export function Widget() {
     /* isWidgetOpen && <p>Hello World</p>  */
 
     return (
-        <Popover className='absolute bottom-5 right-5'>
-            <Popover.Panel>Hello World</Popover.Panel>
+        <Popover className='absolute bottom-4 right-4 md:bottom-10 md:right-8 flex flex-col items-end'>
+            <Popover.Panel>
+                <WidgetForm />
+            </Popover.Panel>
 
             <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6' />
